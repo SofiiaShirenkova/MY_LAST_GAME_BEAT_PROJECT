8ff31aab47265758c7162b32e7789a9f54212d4b
Соня Ночь <144433433+SofiiaShirenkova@users.noreply.github.com>
2024-06-27 22:29:23 +0300

    Add files via upload

---
+++ javascripts/pickle.js
@@ -0,0 +1,96 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const modal = document.getElementById("modal");
+//     const openModalBtn = document.getElementById("openModalBtn");
+//     const closeModalBtn = document.querySelector(".close");
+
+//     // Открыть модальное окно при клике на кнопку
+//     openModalBtn.addEventListener("click", function() {
+//     modal.style.display = "block";
+//     });
+
+//     // Закрыть модальное окно при клике на крестик
+//     closeModalBtn.addEventListener("click", function() {
+//     modal.style.display = "none";
+//     });
+
+//     // Закрыть модальное окно при клике вне его области
+//     window.addEventListener("click", function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+//     });
+
+//     // Отправка формы
+//     const form = document.getElementById("myForm");
+//     form.addEventListener("submit", function(event) {
+//     event.preventDefault(); // Предотвращаем отправку формы
+
+//     // Здесь можно добавить код для отправки данных формы на сервер
+//     // alert("Форма отправлена!");
+//     modal.style.display = "none"; // Закрываем модальное окно после отправки
+//     form.reset(); // Сбрасываем содержимое формы
+//     });
+
+
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Получаем доступ к кнопке
+    const button_cart = document.getElementsByClassName("to_cart");
+
+    for(let i = 0; i < button_cart.length; i++){
+        // Добавляем обработчик события клика
+        button_cart[i].addEventListener("click", function() {
+        // Изменяем текст кнопки
+        button_cart[i].innerText = "В корзине!";
+    });
+    }
+
+    paymentString = "Итого:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
+    prices = {0 : "999"};
+
+
+
+    
+    // КНОПКА КУПИТЬ -- ВЫЛЕЗАЕТ ФОРМА ДЛЯ ЗАПОЛНЕНИЯ
+        const buy_modal = document.getElementById("buy_modal");
+        const openModalBtn = document.getElementsByClassName("buy");
+        const closeModalBtn = document.querySelector(".close");
+        const paymentText = document.getElementById("sum");
+
+
+        for(let i = 0; i < openModalBtn.length; i++){
+            // Открыть модальное окно при клике на кнопку
+            openModalBtn[i].addEventListener("click", function() {
+                buy_modal.style.display = "block";
+                paymentText.innerHTML = paymentString + "&nbsp".repeat(4 - prices[i].length) + prices[i] + "₽";
+            });
+        }
+    
+        // Закрыть модальное окно при клике на крестик
+        closeModalBtn.addEventListener("click", function() {
+            buy_modal.style.display = "none";
+        });
+    
+        // Закрыть модальное окно при клике вне его области
+        window.addEventListener("click", function(event) {
+        if (event.target == buy_modal) {
+            buy_modal.style.display = "none";
+        }
+        });
+    
+        // Отправка формы
+        const form = document.getElementById("buy_myForm");
+        form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Предотвращаем отправку формы
+    
+        // Здесь можно добавить код для отправки данных формы на сервер
+        // alert("Форма отправлена!");
+        buy_modal.style.display = "none"; // Закрываем модальное окно после отправки
+        form.reset(); // Сбрасываем содержимое формы
+        });
+        
+
+});
+
